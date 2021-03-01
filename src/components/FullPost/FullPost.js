f5d854d6e6f6f936c02308ed6e61014c038c5d32
gunaalan <elangogunaalan@gmail.com>
2021-03-01 15:43:07 +0530

    sending first http request to get list of post data and rendering it

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,19 +1,45 @@
 import React, { Component } from "react";
 
 import "./FullPost.css";
+import axios from "axios";
 
 class FullPost extends Component {
+  state = {
+    title: "",
+    content: "",
+    id: "",
+  };
+
+  componentDidUpdate() {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${this.props.id}`)
+      .then((response) => {
+        console.log(response);
+        this.setState({
+          title: response.data.title,
+          content: response.data.body,
+          id: response.data.id,
+        });
+      });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.id !== this.props.id;
+  }
+
   render() {
-    let post = <p>Please select a Post!</p>;
-    post = (
-      <div className="FullPost">
-        <h1>Title</h1>
-        <p>Content</p>
-        <div className="Edit">
-          <button className="Delete">Delete</button>
+    let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
+    if (this.props.id) {
+      post = (
+        <div className="FullPost">
+          <h1>{this.state.title}</h1>
+          <p>{this.state.content}</p>
+          <div className="Edit">
+            <button className="Delete">Delete</button>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
     return post;
   }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,22 +7,45 @@ import "./Blog.css";
 import axios from "axios";
 
 class Blog extends Component {
+  state = {
+    posts: [],
+    selectedPostId: "",
+  };
+
   componentDidMount() {
     axios.get("https://jsonplaceholder.typicode.com/posts").then((response) => {
-      console.log(response);
+      const posts = response.data.slice(0, 6);
+      const updatedPosts = posts.map((post) => {
+        return {
+          ...post,
+          author: "GunaalanElango",
+        };
+      });
+      this.setState({ posts: updatedPosts });
     });
   }
 
+  postSelectable = (postId) => {
+    this.setState({ selectedPostId: postId });
+  };
+
   render() {
+    const posts = this.state.posts.map((post) => {
+      return (
+        <Post
+          clicked={() => this.postSelectable(post.id)}
+          title={post.title}
+          key={post.id}
+          author={post.author}
+        />
+      );
+    });
+
     return (
       <div>
-        <section className="Posts">
-          <Post />
-          <Post />
-          <Post />
-        </section>
+        <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
