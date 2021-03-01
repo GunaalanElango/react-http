76618a775f45d798784ae76a57a0409e5c85a197
gunaalan <elangogunaalan@gmail.com>
2021-03-01 22:14:00 +0530

    using axios instance and default global config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  (request) => {
+    console.log(request);
+    return request;
+  },
+  (error) => {
+    console.log(error);
+    return error;
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log(response);
+    return response;
+  },
+  (error) => {
+    console.log(error);
+    return error;
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
